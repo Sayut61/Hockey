3d2b7dbe684c337d8b1a85c98570a30b7969a9e7
Anton Sayutin <sayutosss@gmail.com>
2021-12-08 15:32:34 +0300

    добавил в TeamPlayersAdapter названия позиций игроков на русском, изменил цвет темы

--- app/src/main/java/com/sayut61/hockey/ui/adapters/TeamPlayersAdapter.kt
+++ app/src/main/java/com/sayut61/hockey/ui/adapters/TeamPlayersAdapter.kt
@@ -14,7 +14,6 @@ class TeamPlayersAdapter(val players: List<TeamPlayersInfo>, val listener: TeamP
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TeamPlayersViewHolder {
         return TeamPlayersViewHolder(TeamPlayersItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
-
     override fun onBindViewHolder(holder: TeamPlayersViewHolder, position: Int) {
         val player = players[position]
         holder.itemView.setOnClickListener {
@@ -22,16 +21,19 @@ class TeamPlayersAdapter(val players: List<TeamPlayersInfo>, val listener: TeamP
         }
         holder.bind(player)
     }
-
     override fun getItemCount(): Int {
         return players.size
     }
 }
-
 class TeamPlayersViewHolder(val binding: TeamPlayersItemBinding): RecyclerView.ViewHolder(binding.root){
     fun bind(player: TeamPlayersInfo){
         binding.namePlayerTextView.text = player.fullName
         binding.numberPlayerTextView.text = player.jerseyNumber.toString()
-        binding.typePlayerTextView.text = player.type
+        binding.typePlayerTextView.text = when(player.type){
+            "Defenseman" -> "Защитник"
+            "Forward" -> "Нападающий"
+            "Goalie" -> "Вратарь"
+            else -> ""
+        }
     }
 }
