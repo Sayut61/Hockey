d95e4c9a6ea6f987e179dacf9ef0c24c56579770
Anton Sayutin <sayutosss@gmail.com>
2021-12-23 16:22:48 +0300

    добавил простые тесты для TeamsRepositoryImplTest.kt

--- app/src/test/java/com/sayut61/hockey/datalayer/repositories/TeamsRepositoryImplTest.kt
+++ app/src/test/java/com/sayut61/hockey/datalayer/repositories/TeamsRepositoryImplTest.kt
@@ -11,6 +11,7 @@ import com.sayut61.hockey.datalayer.datasource.remotedatasource.dto.players.Play
 import com.sayut61.hockey.datalayer.datasource.remotedatasource.dto.players.PlayerStatisticsFromFirstApiResponse
 import com.sayut61.hockey.datalayer.datasource.remotedatasource.dto.stadium.Stadium
 import com.sayut61.hockey.datalayer.datasource.remotedatasource.dto.teams.*
+import com.sayut61.hockey.domain.entities.TeamFullInfo
 import com.sayut61.hockey.domain.entities.TeamGeneralInfo
 import com.sayut61.hockey.domain.entities.TeamPlayersInfo
 import kotlinx.coroutines.runBlocking
@@ -23,7 +24,7 @@ import org.junit.Assert.*
 
 class TeamsRepositoryImplTest {
     lateinit var teamsRepositoryImpl: TeamsRepositoryImpl
-    val remoteDataSource =  FakeDataSource()
+    val remoteDataSource = FakeDataSource()
 
     @Before
     fun setUp() {
@@ -74,13 +75,74 @@ class TeamsRepositoryImplTest {
                 type = "type1"
             ),
             TeamPlayersInfo(
-            jerseyNumber = 2,
-            playerId = 2,
-            fullName = "Igor",
-            linkOnFullInfoByPlayer = "link2",
-            type = "type2"
-        ))
+                jerseyNumber = 2,
+                playerId = 2,
+                fullName = "Igor",
+                linkOnFullInfoByPlayer = "link2",
+                type = "type2"
+            )
+        )
         assertEquals(listPlayer, listPlayersInfo)
+    }
+
+    @Test
+    fun getTeamFullInfo() = runBlocking {
+        val teamId = 1
+        val firstApiInfo = TeamInfoFromFirstApi(
+            id = 1,
+            fullTeamName = "Dallas Stars",
+            shortTeamName = "Stars",
+            officialSiteUrl = "www.nhl.ru",
+            venue = Venue("Stadium")
+        )
+        val secondApiInfo = TeamGeneralInfoFromSecondApi(
+            shortName = "Stars",
+            cityName = "Dallas",
+            wikipediaLogoUrl = "www.logo.ru",
+            stadiumID = 10
+        )
+        val generalInfo = teamsRepositoryImpl.generalInfoToTeamGeneralInfo(firstApiInfo, secondApiInfo)
+        val teamFullInfo = TeamFullInfo(
+            teamGeneralInfo = generalInfo,
+            id = 1,
+            teamFullName = "Dallas Stars",
+            teamShortName = "Stars",
+            firstYearOfPlay = 1900,
+            gamesPlayed = 10,
+            wins = 10,
+            losses = 0,
+            pts = 20,
+            goalsPerGame = 20.0,
+            goalsAgainstPerGame = 10.0,
+            powerPlayPercentage = "10",
+            powerPlayGoals = 20.0,
+            powerPlayGoalsAgainst = 20.0,
+            powerPlayOpportunities = 20.0,
+            shotsPerGame = 10,
+            shotsAllowed = 10,
+            placeOnWins = "20",
+            placeOnLosses = "20",
+            placeOnPts = "20",
+            placeGoalsPerGame = "20",
+            placeGoalsAgainstPerGame = "20"
+        )
+        val result = teamsRepositoryImpl.getTeamFullInfo(teamId)
+        assertEquals(teamFullInfo, result)
+    }
+
+    @Test
+    fun getTeamsInfo() = runBlocking{
+        val teamGeneralInfo = listOf(TeamGeneralInfo(
+            fullTeamName = "Dallas Stars",
+            id = 1,
+            officialSiteUrl = "www.nhl.ru",
+            shortTeamName = "Stars",
+            cityName = "Dallas",
+            urlLogoTeam = "www.logo.ru",
+            stadiumID = 10
+        ))
+        val result = teamsRepositoryImpl.getTeamsInfo()
+        assertEquals(teamGeneralInfo, result)
     }
 
 
@@ -90,10 +152,6 @@ class FakeDataSource() : RemoteDataSource {
 
     override suspend fun getPlayerStatistic(playerId: Int): PlayerStatisticsFromFirstApiResponse {
         TODO("Not yet implemented")
-        /*return when(playerId){
-            1->{}
-            2->{}
-        }*/
     }
 
     override suspend fun getPlayersPhoto(): List<PlayerPhoto> {
@@ -124,7 +182,29 @@ class FakeDataSource() : RemoteDataSource {
     }
 
     override suspend fun getTeamFullInfo(teamId: Int): TeamFullInfoFromApi {
-        TODO("Not yet implemented")
+        return TeamFullInfoFromApi(
+            id = 1,
+            teamFullName = "Dallas Stars",
+            teamShortName = "Stars",
+            firstYearOfPlay = 1900,
+            gamesPlayed = 10,
+            wins = 10,
+            losses = 0,
+            pts = 20,
+            goalsPerGame = 20.0,
+            goalsAgainstPerGame = 10.0,
+            powerPlayPercentage = "10",
+            powerPlayGoals = 20.0,
+            powerPlayGoalsAgainst = 20.0,
+            powerPlayOpportunities = 20.0,
+            shotsPerGame = 10,
+            shotsAllowed = 10,
+            placeOnWins = "20",
+            placeOnLosses = "20",
+            placeOnPts = "20",
+            placeGoalsPerGame = "20",
+            placeGoalsAgainstPerGame = "20"
+        )
     }
 
     override suspend fun getAllPlayers(): List<PlayerGeneralInfoFromApi> {
